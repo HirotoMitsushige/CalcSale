7ef3862e10aac2920cdd109489a558fa34f61ee1
Mitsushige Hiroto <mitsushige.hiroto@m.alhinc.jp>
2017-05-09 16:41:37 +0900

    Calculate Retouching

--- src/jp/alhinc/mitsushige_hiroto/calculate_sales/OpenOfficeFile.java
+++ src/jp/alhinc/mitsushige_hiroto/calculate_sales/OpenOfficeFile.java
@@ -3,7 +3,6 @@ package jp.alhinc.mitsushige_hiroto.calculate_sales;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,81 +17,81 @@ import java.util.Map.Entry;
 public class OpenOfficeFile{
 
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		BufferedReader br=null;
+		BufferedReader br = null;
 		//コマンドライン引数が無いか2つ以上の場合処理を終了。
-		if(args.length==0||args.length>=2){
+		if(args.length != 1){
 			System.out.println("予期せぬエラーが発生しました");
 			return;
 		}
-		//集計用
-		HashMap<String,Long>branchSaleMap=new HashMap<String,Long>();
-		HashMap<String,Long>commoditySaleMap=new HashMap<String,Long>();
-		HashMap<String,String>commodity=new HashMap<String,String>();
-		HashMap<String,String>branch=new HashMap<String,String>();
-		ArrayList<String> rcdList=new ArrayList<String>();
-		ArrayList<Integer> notRcdList=new ArrayList<Integer>();
+		HashMap<String,Long> branchSaleMap = new HashMap<>();
+		HashMap<String,Long> commoditySaleMap = new HashMap<>();
+		HashMap<String,String> commodityMap = new HashMap<>();
+		HashMap<String,String> branchMap = new HashMap<>();
+		ArrayList<String> rcdList = new ArrayList<>();
+		ArrayList<Integer> rcdNameList = new ArrayList<>();
 
-		//支店定義ファイル----------------------------------------------------
-		//blanch.lst
-		if(!(readSaleFile(args[0],"branch.lst","^\\d{3}",
-				"支店定義ファイルのフォーマットが不正です",branch,branchSaleMap))){
+		//支店、商品定義ファイル----------------------------------------------------
+		if(!(readSaleFile(args[0],"branch.lst" , "^\\d{3}",
+				"支店" , branchMap , branchSaleMap))){
 			return;
 		}
-		if(!(readSaleFile(args[0],"commodity.lst","^\\w{8}",
-				"商品定義ファイルのフォーマットが不正です",commodity,commoditySaleMap))){
+		if(!(readSaleFile(args[0] , "commodity.lst" , "^\\w{8}",
+				"商品" , commodityMap , commoditySaleMap))){
 			return;
 		}
 		//集計------------------------------------------------------------------------
 		try{
-			File filler=new File(args[0]);
-			String[] fileList=filler.list();
-			for(int i=0;i<fileList.length;i++){
+			File filler = new File(args[0]);
+			String[] fileList = filler.list();
+			for(int i = 0; i < fileList.length; i++){
 				if(fileList[i].matches("\\d{8}.rcd$")){
 					rcdList.add(fileList[i]);
-					String[] data=fileList[i].split("\\.");
-					notRcdList.add(Integer.parseInt(data[0]));
+					String[] data = fileList[i].split("\\.");
+					rcdNameList.add(Integer.parseInt(data[0]));
 				}
 			}
 			//連番処理はList化し、最大値-最小値＋1する。
-			if(!(notRcdList.size()==(notRcdList.get(notRcdList.size()-1))-(notRcdList.get(0))+1)){
+			if(rcdNameList.size() != rcdNameList.get(rcdNameList.size() -1) - rcdNameList.get(0) + 1){
 				System.out.println("売上げファイル名が連番になっていません");
 				return;
 			}
-			Long Sale=null;
+			Long sale = null;
+			Long sales = null;
 
-			for(int i = 0; i<rcdList.size(); i++){
-				ArrayList<String> rcd=new ArrayList<String>();
-				File file=new File(args[0],rcdList.get(i));
+			for(int i = 0; i< rcdList.size(); i++){
+				ArrayList<String> rcd = new ArrayList<>();
+				File file = new File(args[0],rcdList.get(i));
 
-				br=new BufferedReader(new FileReader(file));
+				br = new BufferedReader(new FileReader(file));
 				String s;
-				while((s=br.readLine())!=null){
+				while((s = br.readLine()) != null){
 					rcd.add(s);
 				}
 
-				//不正処理
-				if(!(rcd.size()==3)){
-					System.out.println(rcdList.get(i)+"のフォーマットが不正です");
+				//フォーマット不正処理
+				if(rcd.size() != 3){
+					System.out.println(rcdList.get(i) + "のフォーマットが不正です");
 					return;
 				}
 				if(!branchSaleMap.containsKey(rcd.get(0))){
-					System.out.println(rcdList.get(i)+"の支店コードが不正です");
+					System.out.println(rcdList.get(i) + "の支店コードが不正です");
 					return;
 				}
-				if(!commodity.containsKey(rcd.get(1))){
-					System.out.println(rcdList.get(i)+"の商品コードが不正です");
+				if(!commodityMap.containsKey(rcd.get(1))){
+					System.out.println(rcdList.get(i) + "の商品コードが不正です");
 					return;
 				}
 
-				Sale = branchSaleMap.get(rcd.get(0)) + Long.parseLong(rcd.get(2));
-				branchSaleMap.put(rcd.get(0),Sale);
-				commoditySaleMap.put(rcd.get(1),Sale);
+				sale = branchSaleMap.get(rcd.get(0)) + Long.parseLong(rcd.get(2));
+				sales = commoditySaleMap.get(rcd.get(1)) + Long.parseLong(rcd.get(2));
+				branchSaleMap.put(rcd.get(0),sale);
+				commoditySaleMap.put(rcd.get(1),sales);
 
 				//金額が10桁を超えたら
-				if(Sale<1000000000L){
-				}else{
+				if(sale > 999999999L){
+
 					System.out.println("合計金額が10桁を超えました");
 					return;
 				}
@@ -101,71 +100,49 @@ public class OpenOfficeFile{
 			System.out.println("予期せぬエラーが発生しました");
 		}finally{
 			try{
-				if(br!=null)
+				if(br != null)
 					br.close();
 			}catch(IOException e){
 				System.out.println("予期せぬエラーが発生しました");
 			}
 		}
-		//支店別集計ファイルと商品別集計ファイル----------------------------------------------------------------------
-
-		if(!(writerFile(args[0],"branch.out",branchSaleMap,branch))){
+		//支店、商品別集計ファイル----------------------------------------------------------------------
+		if(!(writerFile(args[0] , "branch.out"  ,branchSaleMap , branchMap))){
 			return;
 		}
-		if(!(writerFile(args[0],"commodity.out",commoditySaleMap,commodity))){
+		if(!(writerFile(args[0] , "commodity.out" , commoditySaleMap , commodityMap))){
 			return;
 		}
 	}
-	//ファイルチェック------------------------------------------------------------------------------------------------
-	private static boolean checkFile(File file){
-		if (file.isFile()&&file.canRead()){
-			return true;
-		}
-		return false;
-	}
-	//支店定義ファイルと商品定義ファイル------------------------------------------------------------------------------
-	public static boolean readSaleFile(String comLine,String fileLst,String comLineVoid,String errMessage,HashMap<String, String> nameMap,HashMap<String, Long>sales){
-		BufferedReader br=null;
+	//支店、商品定義ファイル------------------------------------------------------------------------------
+	public static boolean readSaleFile(String dirpath , String fileLst , String comLineVoid , String errMessage ,
+			HashMap<String, String> nameMap , HashMap<String, Long> sales){
+		BufferedReader br = null;
 		try{
-			//ファイルを読み込む際は区切りに「￥」を使用するとWindowsのみで動く
-			//仕様となるため、「,」で区切りを入れ、ファイルを読み込む。
-			File file=new File(comLine,fileLst);
-			br=new BufferedReader(new FileReader(file));
-			String str=null;
+			File file = new File(dirpath,fileLst);
+			br = new BufferedReader(new FileReader(file));
+			String str = null;
 
-			//ファイルが読み込めるかチェックする
-			if(checkFile(file)){
-				while((str=br.readLine())!=null){
-					String[] data=str.split(",");
+			while((str = br.readLine()) != null){
+				String[] data = str.split(",");
 
-					//data.lengthで要素数を取得し、要素数が2でない場合はelse(「,」を含む文字列はＮＧ)
-					if(!(data[0].matches(comLineVoid)&&(data[1].matches("\\S+"))&&data.length==2)){
-						System.out.println(errMessage);
-						return false;
-					}
-					if(!(data[0].matches(comLineVoid)&&(data[1].matches("\\S+"))&&data.length==2)){
-						System.out.println(errMessage);
-						return false;
-					}
-					nameMap.put(data[0],data[1]);
-					sales.put(data[0],0L);
+				if(!(data[0].matches(comLineVoid) && (data[1].matches("\\S+")) && data.length == 2)){
+					System.out.println(errMessage + "定義ファイルのフォーマットが不正です");
+					return false;
 				}
-			}else{
-				System.out.println("ファイルを読み込めません");
+				nameMap.put(data[0],data[1]);
+				sales.put(data[0],0L);
 			}
-		}catch(FileNotFoundException e){
-			System.out.println("支店定義ファイルが見つかりません");
-			return false;
 		}catch(IOException e){
 			System.out.println("予期せぬエラーが発生しました");
 			return false;
 		}catch(ArrayIndexOutOfBoundsException e){
-			System.out.println("支店定義ファイルのフォーマットが不正です");
+			System.out.println(errMessage + "定義ファイルのフォーマットが不正です");
 			return false;
 		}
 		//try文においてfinallyは必ず読み込まれるためここでbrを閉じる。
 		finally{
-			if(br!=null)
+			if(br != null)
 				try{
 					br.close();
 					return true;
@@ -177,13 +154,13 @@ public class OpenOfficeFile{
 		return false;
 	}
 	//集計ファイル処理--------------------------------------------------------------------------------------
-	public static boolean writerFile(String dirpath,String fileName ,HashMap<String, Long> sales,HashMap<String, String>nameMap){
+	public static boolean writerFile(String dirpath , String fileName , HashMap<String, Long> sales , HashMap<String, String> nameMap){
 
-		BufferedWriter bw=null;
+		BufferedWriter bw = null;
 		try{
 			File file = new File(dirpath, fileName);
 
-			bw=new BufferedWriter(new FileWriter(file));
+			bw = new BufferedWriter(new FileWriter(file));
 			List<Map.Entry<String,Long>> entries =
 					new ArrayList<Map.Entry<String,Long>>(sales.entrySet());
 			Collections.sort(entries, new Comparator<Map.Entry<String,Long>>() {
@@ -193,15 +170,14 @@ public class OpenOfficeFile{
 				}
 			});
 			for (Entry<String,Long> o : entries) {
-
-				bw.write(o.getKey()+","+nameMap.get(o.getKey())+","+o.getValue());
+				bw.write(o.getKey() + "," + nameMap.get(o.getKey()) + "," + o.getValue());
 				bw.newLine();
 			}
 		}catch(IOException e){
 			System.out.println("予期せぬエラーが発生しました");
 			return false;
 		}finally{
-			if(bw!=null)
+			if(bw != null)
 				try{
 					bw.close();
 					return true;
