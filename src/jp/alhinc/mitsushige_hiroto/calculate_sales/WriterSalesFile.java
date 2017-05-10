0da1b62759953889d698257f4497f71b42a35f79
Mitsushige Hiroto <mitsushige.hiroto@m.alhinc.jp>
2017-05-10 13:56:34 +0900

    Bug Fixes

--- src/jp/alhinc/mitsushige_hiroto/calculate_sales/WriterSalesFile.java
+++ src/jp/alhinc/mitsushige_hiroto/calculate_sales/WriterSalesFile.java
@@ -33,12 +33,12 @@ public class WriterSalesFile{
 		ArrayList<Integer> rcdNameList = new ArrayList<>();
 
 		//支店、商品定義ファイル----------------------------------------------------
-		if(!(readSaleFile(args[0],"branch.lst" , "^\\d{3}",
-				"支店" , branchMap , branchSaleMap))){
+		if(!(readSaleFile(args[0], "branch.lst", "^\\d{3}",
+				"支店", branchMap, branchSaleMap))){
 			return;
 		}
-		if(!(readSaleFile(args[0] , "commodity.lst" , "^\\w{8}",
-				"商品" , commodityMap , commoditySaleMap))){
+		if(!(readSaleFile(args[0], "commodity.lst", "^\\w{8}",
+				"商品", commodityMap, commoditySaleMap))){
 			return;
 		}
 		//集計------------------------------------------------------------------------
@@ -46,23 +46,25 @@ public class WriterSalesFile{
 			File filler = new File(args[0]);
 			String[] fileList = filler.list();
 			for(int i = 0; i < fileList.length; i++){
-				if(fileList[i].matches("\\d{8}.rcd$")){
+				if(checkFile(new File(args[0], fileList[i])) && fileList[i].matches("\\d{8}.rcd$")){
 					rcdList.add(fileList[i]);
 					String[] data = fileList[i].split("\\.");
 					rcdNameList.add(Integer.parseInt(data[0]));
 				}
 			}
 			//連番処理はList化し、最大値-最小値＋1する。
-			if(rcdNameList.size() > 0 && rcdNameList.size() != rcdNameList.get(rcdNameList.size() -1) - rcdNameList.get(0) + 1){
-				System.out.println("売上ファイル名が連番になっていません");
-				return;
+			if(rcdNameList.size() > 0) {
+				if(rcdNameList.size() != rcdNameList.get(rcdNameList.size() -1) - rcdNameList.get(0) + 1){
+					System.out.println("売上ファイル名が連番になっていません");
+					return;
+				}
 			}
 			Long branchsale = null;
 			Long commoditysale = null;
 
 			for(int i = 0; i< rcdList.size(); i++){
 				ArrayList<String> rcd = new ArrayList<>();
-				File file = new File(args[0],rcdList.get(i));
+				File file = new File(args[0], rcdList.get(i));
 				if(!checkFile(file)){
 					System.out.println("予期せぬエラーが発生しました");
 				}
@@ -94,17 +96,17 @@ public class WriterSalesFile{
 				branchsale = branchSaleMap.get(rcd.get(0)) + Long.parseLong(rcd.get(2));
 				commoditysale = commoditySaleMap.get(rcd.get(1)) + Long.parseLong(rcd.get(2));
 				//金額が10桁を超えたら
-				if(branchsale > 999999999L){
+				if(branchsale > 9999999999L){
 					System.out.println("合計金額が10桁を超えました");
 					return;
 				}
-				if(commoditysale > 999999999L){
+				if(commoditysale > 9999999999L){
 					System.out.println("合計金額が10桁を超えました");
 					return;
 				}
 
-				branchSaleMap.put(rcd.get(0),branchsale);
-				commoditySaleMap.put(rcd.get(1),commoditysale);
+				branchSaleMap.put(rcd.get(0), branchsale);
+				commoditySaleMap.put(rcd.get(1), commoditysale);
 			}
 		}catch(IOException e){
 			System.out.println("予期せぬエラーが発生しました");
@@ -117,21 +119,21 @@ public class WriterSalesFile{
 			}
 		}
 		//支店、商品別集計ファイル----------------------------------------------------------------------
-		if(!(writerFile(args[0] , "branch.out"  , branchSaleMap , branchMap))){
+		if(!(writerFile(args[0], "branch.out", branchSaleMap, branchMap))){
 			return;
 		}
-		if(!(writerFile(args[0] , "commodity.out" , commoditySaleMap , commodityMap))){
+		if(!(writerFile(args[0], "commodity.out", commoditySaleMap, commodityMap))){
 			return;
 		}
 	}
 	//支店、商品定義ファイル------------------------------------------------------------------------------
-	public static boolean readSaleFile(String dirpath , String fileLst , String comLineVoid , String errMessage ,
-			HashMap<String, String> nameMap , HashMap<String, Long> sales){
+	public static boolean readSaleFile(String dirPath, String fileLst, String expression, String errMessage,
+			HashMap<String, String> nameMap, HashMap<String, Long> salesMap){
 		BufferedReader br = null;
 		try{
-			File file = new File(dirpath,fileLst);
+			File file = new File(dirPath, fileLst);
 			if(!checkFile(file)){
-				System.out.println(errMessage+"定義ファイルが存在しません");
+				System.out.println(errMessage + "定義ファイルが存在しません");
 				return false;
 			}
 			br = new BufferedReader(new FileReader(file));
@@ -140,12 +142,12 @@ public class WriterSalesFile{
 			while((str = br.readLine()) != null){
 				String[] data = str.split(",");
 
-				if(!(data[0].matches(comLineVoid) && (data[1].matches("\\S+")) && data.length == 2)){
+				if(!(data.length == 2 && data[0].matches(expression))){
 					System.out.println(errMessage + "定義ファイルのフォーマットが不正です");
 					return false;
 				}
-				nameMap.put(data[0],data[1]);
-				sales.put(data[0],0L);
+				nameMap.put(data[0], data[1]);
+				salesMap.put(data[0], 0L);
 			}
 		}catch(IOException e){
 			System.out.println("予期せぬエラーが発生しました");
@@ -167,15 +169,15 @@ public class WriterSalesFile{
 		return true;
 	}
 	//集計ファイル処理--------------------------------------------------------------------------------------
-	public static boolean writerFile(String dirpath , String fileName ,
-			HashMap<String, Long> sales , HashMap<String, String> nameMap){
+	public static boolean writerFile(String dirPath, String fileName,
+			HashMap<String, Long> salesMap, HashMap<String, String> nameMap){
 
 		BufferedWriter bw = null;
 		try{
-			File file = new File(dirpath, fileName);
+			File file = new File(dirPath, fileName);
 			bw = new BufferedWriter(new FileWriter(file));
 			List<Map.Entry<String,Long>> entries =
-					new ArrayList<Map.Entry<String,Long>>(sales.entrySet());
+					new ArrayList<Map.Entry<String,Long>>(salesMap.entrySet());
 
 			Collections.sort(entries, new Comparator<Map.Entry<String,Long>>() {
 				public int compare(Entry<String,Long> entry1, Entry<String,Long> entry2) {
@@ -200,12 +202,12 @@ public class WriterSalesFile{
 		}
 		return true;
 	}
-	 //ファイルチェック
-    private static boolean checkFile(File file){
-             if (file.isFile() && file.canRead() && file.exists()){
-                     return true;
-             }
-             return false;
-     }
+	//ファイルチェック
+	private static boolean checkFile(File file){
+		if (file.isFile() && file.canRead() && file.exists()){
+			return true;
+		}
+		return false;
+	}
 
 }
